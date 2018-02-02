f6c044c2ee834f634a06da15a3ee7265a9e1fa2e
polirex <kekato@miners.utep.edu>
2018-02-02 15:50:30 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+
+
+<body>
+<a href = "../hackathon/health"> Health </a><br>
+<a href = "../hackathon/enterntainment"> Entertainment </a><br>
+<a href = "../hackathon/food"> Food </a><br>
+<a href = "../hackathon/community"> Community </a><br>
+</body>
+<?php
+echo "welcome"
+
+?>
+</html>
